239c3f59abc87e33b8288e608dac7dfa919e8688
Zathras <ChampionMike42@gmail.com>
2021-09-17 16:07:12 -0500

    Add service worker

---
+++ public/service-worker.js
@@ -0,0 +1,79 @@
+const FILES_TO_CACHE = [
+    "/", 
+    "/index.html", 
+    "/index.js", 
+    "/styles.css", 
+    "/icons/icon-192x192.png", 
+    "/icons/icon-512x512.png"
+  ];
+  
+  const CACHE_NAME = "static-cache-v2";
+  const DATA_CACHE_NAME = "data-cache-v1";
+  
+  // install
+  self.addEventListener("install", function (e) {
+    e.waitUntil(
+      caches.open(CACHE_NAME).then(cache => {
+        console.log("Your files were pre-cached successfully!");
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+  
+    self.skipWaiting();
+  });
+  
+  // activate
+  self.addEventListener("activate", function (e) {
+    e.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
+  
+  // fetch
+  self.addEventListener("fetch", function (e) {
+    if (e.request.url.includes("/api/")) {
+      e.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(e.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(e.request.url, response.clone());
+              }
+  
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(e.request);
+            });
+        }).catch(err => console.log(err))
+      );
+  
+      return;
+    }
+  
+    e.respondWith(
+      fetch(e.request).catch(function () {
+        return caches.match(e.request).then(function (response) {
+          if (response) {
+            return response;
+          } else if (e.request.headers.get("accept").includes("text/html")) {
+            // return the cached home page for all requests for html pages
+            return caches.match("/");
+          }
+        });
+      })
+    );
+  });
