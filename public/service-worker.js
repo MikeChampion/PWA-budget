1978bc35dda84b790364dbde163bbfc03efb136d
Zathras <ChampionMike42@gmail.com>
2021-09-17 16:32:07 -0500

    Update service-worker and manifest

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,14 +1,16 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
     "/", 
     "/index.html", 
     "/index.js", 
     "/styles.css", 
+    "/manifest.webmanifest",
     "/icons/icon-192x192.png", 
     "/icons/icon-512x512.png"
   ];
   
-  const CACHE_NAME = "static-cache-v2";
-  const DATA_CACHE_NAME = "data-cache-v1";
+
   
   // install
   self.addEventListener("install", function (e) {
